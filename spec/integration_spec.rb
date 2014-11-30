5a698c5940ab49509d719b75a7758a8b270bed55
John Davison <johncdavison@gmail.com>
2014-11-30 13:30:57 -0700

    Increase spec coverage.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 
 describe 'LendBot + Community' do
   include_context 'create and populate community'
+
+  before do
+    args = {lendee: @alice, amount: 1000, community: @community}
+    LendBot.generate_proportional_loans(args)
+  end
+
   context 'Community' do
     it '#primary_network_net_value' do
-      args = {lendee: @alice, amount: 1000, community: @community}
-      LendBot.generate_proportional_loans(args)
       expect(@community.primary_network_net_value 'betty').to eq 2070
+    end
+
+    it '#primary_network_sum_loans_lent' do
+      expect(@community.primary_network_sum_loans_lent 'alice').to eq 1000
     end
   end
 end
